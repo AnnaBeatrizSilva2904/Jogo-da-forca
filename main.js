47b2b41f3c3a6dd3338eaee3bd1e14c8476275a1
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2024-10-10 12:25:29 -0300

    Começo do projeto

---
+++ main.js
@@ -0,0 +1,5 @@
+const palavras = ["abacate", "batata", "gato"];
+let tentativasUsadas = 0;
+let letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+let palavraSecreta, palavraOcula;
+let jogando;
